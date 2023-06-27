7e12f1256d555c392e0e968c6953d75e7f507b52
JD <jayprowings@gmail.com>
2023-06-27 10:43:18 +0530

    27 June - session methods

---
+++ src/main/java/com/prowings/entity/Student2.java
@@ -0,0 +1,78 @@
+package com.prowings.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class Student2 {
+	@Id
+	int id;
+	@Column(name = "STD_Roll")
+	int roll;
+	@Column
+	String name;
+	@Column(name = "STD_Address")
+	String address;
+
+	public Student2() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Student2(int id, int roll, String name, String address) {
+		super();
+		this.id = id;
+		this.roll = roll;
+		this.name = name;
+		this.address = address;
+	}
+
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", roll=" + roll + ", name=" + name + ", address=" + address + "]";
+	}
+
+	
+
+}
--- src/main/java/com/prowings/main/TestHibernateApp.java
+++ src/main/java/com/prowings/main/TestHibernateApp.java
@@ -6,15 +6,17 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.prowings.entity.Student;
+import com.prowings.entity.Student2;
 
 public class TestHibernateApp {
 	
 	public static void main(String[] args) {
 		
-//		Student std1 = new Student();
-//		std1.setRoll(30);
-//		std1.setName("AAA");
-//		std1.setAddress("Dubai");
+		Student2 std1 = new Student2();
+		std1.setId(4);
+		std1.setRoll(123);
+		std1.setName("DDD122");
+		std1.setAddress("XXX122");
 		
 		Configuration conf = new Configuration();
 		
@@ -26,15 +28,21 @@ public class TestHibernateApp {
 		
 		Transaction txn = session.beginTransaction();
 		
-//		session.save(std1);
+//		System.out.println(">>>>>>>>save method returned : " +session.save(std1));
+//		session.saveOrUpdate(std1);
+		
+//		session.persist(std1);
+		
+//		session.delete(std1);
+//		session.remove(std1);
 		
 //		Student s = session.get(Student.class, 1);
-		Student s = session.load(Student.class, 10);
+//		Student s = session.load(Student.class, 10);
 		
 		txn.commit();
 		
 		
-		System.out.println("student data from DB for id 1 : "+s);
+//		System.out.println("student data from DB for id 1 : "+s);
 		System.out.println("student object saved to DB successfully!!!");
 		session.close();
 		
